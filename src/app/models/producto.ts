164422e8ac29ce88fbcac571c7d59d99beacf263
serraguti <pacoserranox@gmail.com>
2021-11-15 10:09:29 +0100

    commit

--- src/app/models/producto.ts
+++ src/app/models/producto.ts
@@ -1,13 +1,8 @@
 export class Producto {
-    public nombre: string;
-    public imagen: string;
-    public precio: number;
-
-    //CREAMOS UN CONSTRUCTOR PARA RECIBIR
-    //PARAMETROS
-    constructor (nombre: string, imagen: string, precio: number) {
-        this.nombre = nombre;
-        this.imagen = imagen;
-        this.precio = precio;
-    }
+    constructor (
+        public nombre: string,
+        public imagen: string,
+        public precio: number
+    ) {}
 }
+
